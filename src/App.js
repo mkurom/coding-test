24c4eeb5d067c76cc85c2b98b080fd802f15599c
yasudamakoto <hare.v1v1enne@gmail.com>
2022-04-16 20:04:35 +0900

    サンプルデータでリスト作成

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 
+import { TodoList } from './TodoList';
+
 export const App = () => {
+
+
   return (
     <div>
       <h1>TODOアプリ</h1>
@@ -12,6 +16,7 @@ export const App = () => {
         </label>
         <input type="submit" value="作成" />
       </form>
+      <TodoList />
     </div>
   );
 }
