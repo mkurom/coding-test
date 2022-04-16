8f11054fb6524080988d67f02cb6056ce343a70a
yasudamakoto <hare.v1v1enne@gmail.com>
2022-04-16 19:26:22 +0900

    form作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>TODOアプリ</h1>
+
+      <form>
+        <label>
+          TODO:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="作成" />
+      </form>
     </div>
   );
 }
-
-export default App;
