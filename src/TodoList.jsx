3ec428ebce2c9a58cd3d6a2df600d2382f37cac7
yasudamakoto <hare.v1v1enne@gmail.com>
2022-04-19 00:29:00 +0900

    todoのフィルターを追加

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,29 +1,59 @@
-// import { AiOutlineCheck } from "react-icons/fa"
+import { completeTodo, incompleteTodo } from './constant.js';
 import React from "react";
 
 export const TodoList = (props) => {
-
   const { todoItems, onClickDone, onClickDelete } = props;
+
+  const [todoFilter, setTodoFilter] = React.useState(0);
+  const onClickFilter = (filterIndex) => {
+    setTodoFilter(filterIndex);
+  };
 
   if (todoItems === undefined) return;
 
-  const todoList = todoItems.map((item, index) =>
-    <li key={item.text}>
-      <div>
-        {item.text}
-        {item.done && " 完了済"}
+  var filterdList = [];
+  const newTodoList = [...todoItems];
+
+  if (todoFilter === completeTodo) {
+    filterdList = newTodoList.filter((todoItem) => {
+      return todoItem.done;
+    });
+  } else if (todoFilter === incompleteTodo) {
+    filterdList = newTodoList.filter((todoItem) => {
+      return !todoItem.done;
+    });
+  } else {
+    filterdList = newTodoList;
+  }
+
+  const todoList = filterdList.map((item, index) => {
+    return (
+      <li key={item.text}>
         <div>
-          <button type="button" onClick={() => onClickDone(index)}>{
-            item.done ? "未完了" : "完了"
-          }</button>
-          <button type="button" onClick={() => onClickDelete(index)}>削除</button>
+          {item.text}
+          {item.done && " 完了済"}
+          <div>
+            <button type="button" onClick={() => onClickDone(index)}>{
+              item.done ? "未完了" : "完了"
+            }</button>
+            <button type="button" onClick={() => onClickDelete(index)}>削除</button>
+          </div>
         </div>
-      </div>
-    </li>
-  );
+      </li>
+    );
+  });
 
   return (
     <div>
+      {todoItems.length > 0 &&
+        (
+          <div onChange={(e) => onClickFilter(Number(e.target.value))}>
+            <input type="radio" name="theme" value="0" />全て
+            <input type="radio" name="theme" value="1" />完了
+            <input type="radio" name="theme" value="2" />未完了
+          </div>
+        )
+      }
       <p>Todo件数: {todoItems.length} 件</p>
       <ul>{todoList}</ul>
     </div>
---
+++ src/constant.js
@@ -0,0 +1,5 @@
+
+// todoのフィルター用
+export const allTodo = 0;
+export const completeTodo = 1;
+export const incompleteTodo = 2;
